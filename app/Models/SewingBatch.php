57ab042eae72f6b94bef96cc715f9c579975a38f
zuares <zuaresquad@gmail.com>
2025-11-16 06:15:23 +0700

    tambah model dan migrasi sewing_batch dan sewing_bundle_lines

---
+++ app/Models/SewingBatch.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SewingBatch extends Model
+{
+    protected $fillable = [
+        'code',
+        'production_batch_id',
+        'employee_id',
+        'status',
+        'total_qty_input',
+        'total_qty_ok',
+        'total_qty_reject',
+        'started_at',
+        'finished_at',
+    ];
+
+    public function productionBatch()
+    {
+        return $this->belongsTo(ProductionBatch::class);
+    }
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+
+    public function lines()
+    {
+        return $this->hasMany(SewingBundleLine::class);
+    }
+}
